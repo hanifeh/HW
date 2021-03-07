3f0838ada963ef0bdd210d5af14f4a133c3fd82e
wizard hanifeh <s.hanifehnia@gmail.com>
2021-03-07 23:21:49 +0330

    eslahe commsions

--- HW6/Sadeghhanifehnia_HW6_Maktab50.py
+++ HW6/Sadeghhanifehnia_HW6_Maktab50.py
@@ -17,14 +17,14 @@ def calculate_commission(product_id, count, user_id):
     commissions_data = [(d_cost(group), d_unit(group)) for group in commission_groups for userid in
                         d_users(group) if userid == user_id]
     if len(commissions_data) == 0:
-        return total
+        return 0
     else:
         for commission in commissions_data:
             if commission[1] == 'percent':
-                commissions.append(total - (total * commission[0] / 100))
+                commissions.append(total * commission[0] / 100)
             if commission[1] == 'Dollar':
-                commissions.append(total - (count * commission[0]))
-    commissions_sorted = sorted(commissions, reverse=True)
+                commissions.append(count * commission[0])
+    commissions_sorted = sorted(commissions)
     return commissions_sorted[0]
 
 
@@ -41,10 +41,8 @@ def calculate_markup_percent(product_id, count):
 def calculate_product_price(product_id, count, user_id=0):
     product_name = p_name(product_id)
     total_price = round(calculate_total_price(product_id, count), 2)
-    total_with_commission = total_price
-    if user_id > 1000:
-        total_with_commission = round(calculate_commission(product_id, count, user_id), 2)
-    discount = round(total_price - total_with_commission, 2)
+    discount = round(calculate_commission(product_id, count, user_id), 2)
+    total_with_commission = round(total_price - discount, 2)
     user_name = u_names(user_id)
     result = {'product_name': product_name, 'total_price': total_price, 'total_with_commission': total_with_commission,
               'discount': discount, 'username': user_name}
@@ -56,5 +54,8 @@ print(calculate_markup_percent("2", 1))
 print(calculate_markup_percent("3", 20))
 print(calculate_product_price("1", 8, 1002))
 print(calculate_product_price("1", 7, 1003))
-print(calculate_product_price("4", 3, 1005))
+print(calculate_product_price("3", 3, 1004))
 print(calculate_product_price("2", 1))
+print(calculate_product_price("1", 10, 1002))
+print(calculate_product_price("1", 15, 1003))
+print(calculate_product_price("4", 20, 1005))
