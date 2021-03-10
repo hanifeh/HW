9504e063d7998f3ac7348649d2dbee330944863d
wizard hanifeh <s.hanifehnia@gmail.com>
2021-03-10 19:11:41 +0330

    try clean code

--- HW6/Sadeghhanifehnia_HW6_Maktab50.py
+++ HW6/Sadeghhanifehnia_HW6_Maktab50.py
@@ -5,27 +5,28 @@ from user_data import u_names
 
 
 def calculate_total_price(product_id, count):
-    product_start_price = p_price(product_id)
+    start_price = p_price(product_id)
     markup = calculate_markup_percent(product_id, count)
-    return (product_start_price + (product_start_price * markup / 100)) * count
+    total_price = round((start_price + (start_price * markup / 100)) * count, 2)
+    return total_price
 
 
-def calculate_commission(product_id, count, user_id):
-    total = calculate_total_price(product_id, count)
+def calculate_discount(product_id, count, user_id):
+    total_price = calculate_total_price(product_id, count)
     commissions = []
     commission_groups = p_commission(product_id)
-    commissions_data = [(d_cost(group), d_unit(group)) for group in commission_groups for userid in
-                        d_users(group) if userid == user_id]
+    commissions_data = [(d_cost(group), d_unit(group)) for group in commission_groups
+                        for userid in d_users(group) if userid == user_id]
     if len(commissions_data) == 0:
-        return 0
+        discount = 0
     else:
         for commission in commissions_data:
             if commission[1] == 'percent':
-                commissions.append(total * commission[0] / 100)
+                commissions.append(total_price * commission[0] / 100)
             elif commission[1] == 'Dollar':
                 commissions.append(commission[0])
-    commissions_sorted = sorted(commissions, reverse=True)
-    return commissions_sorted[0]
+        discount = round(sorted(commissions, reverse=True)[0], 2)
+    return discount
 
 
 def calculate_markup_percent(product_id, count):
@@ -33,28 +34,34 @@ def calculate_markup_percent(product_id, count):
     upper_cost = mup_upper_cost(product_id)
     lower_count = mup_lower_count(product_id)
     if count > lower_count:
-        return lower_cost
+        markup = lower_cost
     else:
-        return round(upper_cost - ((count - 1) * ((upper_cost - lower_cost) / (lower_count - 1))), 3)
+        markup = round(upper_cost - ((count - 1) * ((upper_cost - lower_cost) / (lower_count - 1))), 3)
+    return markup
 
 
 def calculate_product_price(product_id, count, user_id=0):
     product_name = p_name(product_id)
-    total_price = round(calculate_total_price(product_id, count), 2)
-    discount = round(calculate_commission(product_id, count, user_id), 2)
+    total_price = calculate_total_price(product_id, count)
+    discount = calculate_discount(product_id, count, user_id)
     total_with_commission = round(total_price - discount, 2)
     user_name = u_names(user_id)
-    result = {'product_name': product_name, 'total_price': total_price, 'total_with_commission': total_with_commission,
-              'discount': discount, 'username': user_name}
+    result = {
+        'product_name': product_name,
+        'total_price': total_price,
+        'total_with_commission': total_with_commission,
+        'discount': discount,
+        'username': user_name,
+    }
     return result
 
 
 print(calculate_markup_percent("1", 5))
-print(calculate_markup_percent("2", 1))
+print(calculate_markup_percent("2", 3))
 print(calculate_markup_percent("3", 20))
-print(calculate_product_price("1", 8, 1002))
-print(calculate_product_price("1", 7, 1003))
-print(calculate_product_price("3", 3, 1004))
+print(calculate_product_price("1", 5, 1002))
+print(calculate_product_price("2", 5, 1003))
+print(calculate_product_price("3", 2, 1004))
 print(calculate_product_price("2", 1))
 print(calculate_product_price("1", 10, 1002))
 print(calculate_product_price("1", 15, 1003))
