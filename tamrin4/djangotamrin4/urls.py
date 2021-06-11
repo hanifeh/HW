8bfc2acce5d0a740dff665f976c245443ccd6901
hanifeh <sadeghhanifehnia@gmail.com>
2021-06-11 14:45:47 +0430

    add tamrin4

---
+++ tamrin4/djangotamrin4/urls.py
@@ -0,0 +1,29 @@
+"""djangotamrin4 URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from user.views import CreateUser, Search, NewSearch, EditNumber
+from django.views.generic.base import TemplateView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('create/', CreateUser.as_view(), name='create'),
+    path('search/', Search.as_view(), name='search'),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('newsearch/', NewSearch.as_view(), name='new search'),
+    path('editnumber/<int:pk>', EditNumber.as_view(), name='edit'),
+]
---
+++ tamrin4/user/forms.py
@@ -0,0 +1,24 @@
+from django import forms
+from django.core.exceptions import ValidationError
+
+from . import models
+
+
+class UserForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        self.user = kwargs.pop('user')
+        super(UserForm, self).__init__(*args, **kwargs)
+
+    def clean_phone_number(self):
+        phone_number = self.cleaned_data['phone_number']
+        if models.MyUser.objects.filter(user=self.user, phone_number=phone_number).exists():
+            raise ValidationError('omg')
+        return phone_number
+
+    class Meta:
+        model = models.MyUser
+        fields = [
+            'first_name',
+            'last_name',
+            'phone_number',
+        ]
