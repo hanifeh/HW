2b50e145ae91d25d5afe334bdc4ca4f205e26a3a
sadegh hanifeh nia <79929375+hanifeh@users.noreply.github.com>
2021-06-11 15:03:10 +0430

    Update views.py

--- tamrin4/user/views.py
+++ tamrin4/user/views.py
@@ -29,7 +29,7 @@ class CreateUser(LoginRequiredMixin, CreateView):
 
     def form_valid(self, form):
         form.instance.user = self.request.user
-        f = super().form_vali(form)
+        f = super().form_valid(form)
         return JsonResponse({'status': "ok"})
 
 
